ef4f80b7785a6a0183c3eaa633bec6f4c66a4ac7
boxmars <2803726925@qq.com>
2020-07-26 23:15:29 +0800

    Update

--- Vaughan/settings.py
+++ Vaughan/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'tm@@x$4gu&u6g$(6#3+)5d$sag!_m%pja=koqt!t7hf^&#sw*1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
